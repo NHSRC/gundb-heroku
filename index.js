8367d955e73903d1a64e0d04e1e460075e18c8a9
mihirk <mihir.khatwani@gmail.com>
2016-10-12 11:21:53 +0530

    Running Gun DB

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
+var Gun = require('gun');
+var gun = Gun({file: false});
+var port = process.env.PORT || process.argv[2] || 8080;
+var ip = '0.0.0.0';
+var http = require('http');
+var server = http.createServer();
+gun.wsp(server);
+server.listen(port, '0.0.0.0');
+console.log("Server running on " + ip + ":" + port);
